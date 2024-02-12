abc7ea924389ec2bff5cb941daf49441f0707328
Camille <83260908+Camille846@users.noreply.github.com>
2024-02-12 15:15:21 -0300

    create components setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { LeftCardComponent } from './components/left-card/left-card.component';
+import { RightCardComponent } from './components/right-card/right-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuBarComponent, MenuTitleComponent, LeftCardComponent, RightCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/left-card/left-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './left-card.component.html',
+  styleUrl: './left-card.component.css'
+})
+export class LeftCardComponent {
+
+}
